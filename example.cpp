5fc5398c365ad8dcd9c1f6f2ed7cfe284e110a08
mourisl <mourisl@hotmail.com>
2025-02-14 21:19:20 -0500

    Add the functionality to support parsing tab,space-splited text, which can be used in parsing barcode,umi put in the header of a read fasta/fastq file

--- example.cpp
+++ example.cpp
@@ -8,6 +8,7 @@ int main(int argc, char *argv[])
 {
   ReadFormatter formatter ;
   char read[] = "ATCGATCGATCGATCG" ;
+	char comment[] = "CB:Z:ACGT-1 CB:Z:TGCA-2 UB:Z:GGGG" ;
   char qual[] = "ABCDEFGHIJKLMOPQ" ;
 
   formatter.Init(argv[1]) ;
@@ -15,10 +16,20 @@ int main(int argc, char *argv[])
       formatter.Extract(qual, FORMAT_READ1, false, true, 1)) ;
   printf("@r2\n%s\n-\n%s\n", formatter.Extract(read, FORMAT_READ2, true, true),
       formatter.Extract(qual, FORMAT_READ2, false, true, 1)) ;
-  printf("@bc\n%s\n-\n%s\n", formatter.Extract(read, FORMAT_BARCODE, true, true),
+  
+  if (!formatter.IsInComment(FORMAT_BARCODE))
+    printf("@bc\n%s\n-\n%s\n", formatter.Extract(read, FORMAT_BARCODE, true, true),
       formatter.Extract(qual, FORMAT_BARCODE, false, true, 1)) ;
-  printf("@um\n%s\n-\n%s\n", formatter.Extract(read, FORMAT_UMI, true, true),
+  else
+    printf("@bc\n%s\n-\n%s\n", formatter.Extract(comment, FORMAT_BARCODE, true, true),
+      formatter.Extract(comment, FORMAT_BARCODE, false, true, 1)) ;
+
+  if (!formatter.IsInComment(FORMAT_UMI))
+    printf("@um\n%s\n-\n%s\n", formatter.Extract(read, FORMAT_UMI, true, true),
       formatter.Extract(qual, FORMAT_UMI, false, true, 1)) ;
+  else
+    printf("@um\n%s\n-\n%s\n", formatter.Extract(comment, FORMAT_UMI, true, true),
+      formatter.Extract(comment, FORMAT_UMI, false, true, 1)) ;
   
   return 0 ;
 }
